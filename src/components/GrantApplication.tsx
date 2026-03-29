d024bead6430d6d68b14dee394f95c3444594580
LeistaManu <148265826+LeistaManu@users.noreply.github.com>
2026-03-29 16:52:57 +0300

    Update GrantApplication.tsx

--- src/components/GrantApplication.tsx
+++ src/components/GrantApplication.tsx
@@ -127,7 +127,7 @@ const GrantApplication = () => {
       `Registration Fee: ${registrationFee.toFixed(2)} ${displayCurrency.currency}%0A` +
       `Purpose: ${formData.purpose}`;
 
-    window.location.href = `https://wa.me/+254769999807?text=${message}`;
+    window.location.href = `https://wa.me/+254718663469?text=${message}`;
   };
 
   const renderStep = () => {
